4baf7f08df5c2c418fc2ae148a7ee74ef8b54d4a
Burak Caglayan <burak.caglayan@trendyol.com>
2020-06-13 11:22:27 +0300

    add - delete environment without redux

--- app/components/enviroment/EnvironmentEdit.tsx
+++ app/components/enviroment/EnvironmentEdit.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Environment.css';
 import {Link} from "react-router-dom";
 import routes from "../../constants/routes.json";
-import { FormGroup, InputGroup} from "@blueprintjs/core";
+import {Button, FormGroup, InputGroup} from "@blueprintjs/core";
 import EnvironmentList from "./EnvironmentList";
+import {AppToaster} from "../../toast/toaster";
 
 export default function EnvironmentEdit() {
+  const [environment, setEnvironment] = useState("");
+  const [url, setUrl] = useState("");
+  const saveEnvironment = () => {
+    let environments = JSON.parse(localStorage.getItem("environments"));
+    var env = {name: environment, url: url};
+
+    environments.push(env);
+    localStorage.setItem("environments", JSON.stringify(environments));
+
+    AppToaster.show({message: "Environment added.", icon: "confirm", intent: "success"});
+  }
+
   return (
     <div data-tid="container" className={styles.container}>
       <Link to={routes.HOME}>
-        <i className="fa fa-arrow-left fa-3x" />
+        <i className="fa fa-arrow-left fa-3x"/>
       </Link>
       <h4>Add Environment</h4>
       <FormGroup
@@ -18,7 +31,7 @@ export default function EnvironmentEdit() {
         labelInfo="(required)"
         className={`${styles.myClass} bp3-dark`}
       >
-        <InputGroup id="text-input" placeholder="Environment" />
+        <InputGroup id="text-input" placeholder="Environment" onChange={e => setEnvironment(e.target.value)}/>
       </FormGroup>
 
       <FormGroup
@@ -27,10 +40,13 @@ export default function EnvironmentEdit() {
         labelInfo="(required)"
         className={`${styles.myClass} bp3-dark`}
       >
-        <InputGroup id="text-input" placeholder="Url" />
+        <InputGroup id="text-input" placeholder="Url" onChange={e => {
+          setUrl(e.target.value)
+        }}/>
+        <Button className={styles.saveButton} icon={"saved"} onClick={saveEnvironment}>Save</Button>
       </FormGroup>
 
-     <EnvironmentList />
+      <EnvironmentList/>
     </div>
   );
 }
--- app/components/enviroment/EnvironmentList.tsx
+++ app/components/enviroment/EnvironmentList.tsx
@@ -1,13 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import styles from './Environment.css';
-import { Cell, Column, Table } from "@blueprintjs/table";
+import {Cell, Column, Table} from "@blueprintjs/table";
 import {Button} from "@blueprintjs/core";
-
+import {HeaderCell} from "@blueprintjs/table/lib/cjs/headers/headerCell";
+import {AppToaster} from "../../toast/toaster";
 
 export default function EnvironmentList() {
-  const [environments,setEnvironments]=useState([]);
+  const [environments, setEnvironments] = useState([]);
 
-  const cellRendererName = (rowIndex: number) => {
+  const deleteEnvironment = (deleteEnvironment) => {
+    let environments = JSON.parse(localStorage.getItem("environments"));
+
+    var deleteIndex = environments.findIndex(x => x.name == deleteEnvironment);
+    environments.splice(deleteIndex, 1);
+    setEnvironments(environments);
+    localStorage.setItem("environments", JSON.stringify(environments));
+    AppToaster.show({message: "Environment deleted.", icon: "confirm", intent: "success"});
+  }
+
+  const cellRendererEnvironment = (rowIndex: number) => {
     return <Cell>{environments[rowIndex].name}</Cell>
   };
 
@@ -15,21 +26,36 @@ export default function EnvironmentList() {
     return <Cell>{environments[rowIndex].url}</Cell>
   };
   const cellRendererDelete = (rowIndex: number) => {
-    return <Cell><Button class="bp3-button"  icon="refresh" intent="danger" text="Delete" /></Cell>
+    return <Cell><Button className="bp3-button" icon="delete" intent="danger" text=""
+                         onClick={() => deleteEnvironment(environments[rowIndex].name)}/></Cell>
   };
 
-  useEffect(()=>{
-    let list= JSON.parse(localStorage.getItem("environments"));
+  const environmentHeaderCellRenderer = () => {
+    return <HeaderCell>Environment</HeaderCell>
+  };
+
+  const urlHeaderCellRenderer = () => {
+    return <HeaderCell>Url</HeaderCell>
+  };
+
+  const deleteHeaderCellRenderer = () => {
+    return <HeaderCell>#</HeaderCell>
+  };
+
+  useEffect(() => {
+    let list = JSON.parse(localStorage.getItem("environments"));
     setEnvironments(list);
-  },[])
+  }, []);
 
   return (
-    <div data-tid="table-container" className={styles.container}>
+    <div data-tid="table-container" className={styles.tablecontainer}>
 
-      <Table  numRows={environments.length} className={`${styles.myClass} bp3-dark`}>
-        <Column name="Environment" cellRenderer={cellRendererName}/>
-        <Column name="Url" cellRenderer={cellRendererUrl}/>
-        <Column name="#" cellRenderer={cellRendererDelete}/>
+      <Table numRows={environments.length} defaultRowHeight={35} columnWidths={[150, 300, 50]} minColumnWidth={20}
+             className={`${styles.myClass} bp3-dark`}>
+        <Column name="Environment" cellRenderer={cellRendererEnvironment}
+                columnHeaderCellRenderer={environmentHeaderCellRenderer}/>
+        <Column name="Url" cellRenderer={cellRendererUrl} columnHeaderCellRenderer={urlHeaderCellRenderer}/>
+        <Column name="#" cellRenderer={cellRendererDelete} columnHeaderCellRenderer={deleteHeaderCellRenderer}/>
       </Table>
     </div>
   );

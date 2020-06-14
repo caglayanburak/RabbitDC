3158b8578216163d22848408d5e153f0bcd3e408
Burak Caglayan <burak.caglayan@trendyol.com>
2020-06-14 15:01:59 +0300

    add - delete - change environment with redux

--- app/reducers/types.ts
+++ app/reducers/types.ts
@@ -1,7 +1,9 @@
-import { Dispatch as ReduxDispatch, Store as ReduxStore, Action } from 'redux';
+import {Dispatch as ReduxDispatch, Store as ReduxStore, Action} from 'redux';
 
 export type counterStateType = {
   counter: number;
+  environments: any[];
+  currentEnvironment: string
 };
 
 export type GetState = () => counterStateType;

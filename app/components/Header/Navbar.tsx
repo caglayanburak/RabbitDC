85d9a532459c6eac67d7e5b67adc941960d9d815
Burak Caglayan <burak.caglayan@trendyol.com>
2020-06-17 23:22:07 +0300

    add vhosts

--- app/components/Header/Navbar.tsx
+++ app/components/Header/Navbar.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import {Environment} from "../enviroment/Environment";
-import {Button, Menu, MenuDivider, MenuItem, Popover} from "@blueprintjs/core";
+import {Button, Menu, MenuItem, Popover} from "@blueprintjs/core";
 import {Position} from "@blueprintjs/core/lib/esm/common/position";
 import {Link} from "react-router-dom";
 import routes from "../../constants/routes.json";
+import {Vhosts} from "../enviroment/Vhosts";
+import {useEffect} from "react";
 
-export const Navbar = () => {
+type Props = {
+  change: (payload: any) => void,
+  changeVhosts: (payload: any) => void,
+  getVhosts: () => void,
+  getCurrentVhost: () => void,
+  vHosts: any[],
+  currentVhost: string
+};
+
+export const Navbar = ({change, getVhosts, vHosts, changeVhosts, currentVhost, getCurrentVhost}: Props) => {
+  useEffect(() => {
+    getVhosts();
+    getCurrentVhost();
+  }, []);
+
   return (
     <nav className="bp3-navbar bp3-dark">
       <div>
@@ -19,18 +35,19 @@ export const Navbar = () => {
           <button className="bp3-button bp3-minimal bp3-icon-document">
             Error Queues
           </button>
-          <Environment />
-          <span className="bp3-navbar-divider" />
-          <button className="bp3-button bp3-minimal bp3-icon-user" />
-          <button className="bp3-button bp3-minimal bp3-icon-notifications" />
+          <Vhosts changeVhosts={changeVhosts} vHosts={vHosts} currentVhost={currentVhost}/>
+          <Environment change={change}/>
+          <span className="bp3-navbar-divider"/>
+          <button className="bp3-button bp3-minimal bp3-icon-user"/>
+          <button className="bp3-button bp3-minimal bp3-icon-notifications"/>
           <Popover content={<Menu>
-              <Link to={routes.EnvironmentEdit}>
-                <MenuItem icon="graph" text="Environments">
-                </MenuItem>
-              </Link>
+            <Link to={routes.EnvironmentEdit}>
+              <MenuItem icon="graph" text="Environments">
+              </MenuItem>
+            </Link>
 
           </Menu>} position={Position.BOTTOM_LEFT}>
-            <Button className="bp3-button bp3-minimal bp3-icon-cog" />
+            <Button className="bp3-button bp3-minimal bp3-icon-cog"/>
           </Popover>
         </div>
       </div>
